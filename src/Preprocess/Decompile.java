2881488c13718c01388ca16e12623de4c3d367ae
WHATSUPTOYOU <997762515@qq.com>
2022-06-24 17:38:38 +0800

    patch

--- src/Preprocess/Decompile.java
+++ src/Preprocess/Decompile.java
@@ -5,9 +5,16 @@ import Configure.RuntimeConfig;
 import java.io.IOException;
 
 public class Decompile {
-    public static void decmp(String classFile) throws IOException {
+    public static void decmp(String classFile) throws IOException, InterruptedException {
         String cmd = "java -jar ./lib/procyon-decompiler-0.5.36.jar " + classFile + " > " + RuntimeConfig.runtimeTfile;
-        Runtime.getRuntime().exec(cmd);
+        Process process;
+        process = Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", cmd});
+        try {
+            process.waitFor();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+//        System.out.println(process);
     }
 
     public static void unzip(String tPath) throws IOException {
